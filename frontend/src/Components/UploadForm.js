26286995459a13e1c9f0a3238cc5d0152d28b397
Alexander Grattan <alex@grattan.me>
2021-02-20 10:04:22 -0500

    feat: get resume form and axios

---
+++ frontend/src/Components/UploadForm.js
@@ -0,0 +1,56 @@
+import React from "react";
+import axios from "axios";
+
+import { useFormik } from "formik";
+
+function UploadForm() {
+  const formik = useFormik({
+    initialValues: {
+      file: null,
+    },
+    onSubmit: (values) => {
+      console.log(values);
+      axios
+        .post(`/`, {
+          file: values.file,
+        })
+        .then((res) => {
+          console.log(res);
+        });
+    },
+    // validate: (values) => {
+    //   let errors = {};
+
+    //   if (!values.file) {
+    //     errors.file = "Required";
+    //   }
+    //   return errors;
+    // },
+  });
+  return (
+    <form
+      onSubmit={formik.handleSubmit}
+      className="uploadForm"
+      name="uploadForm"
+    >
+      <label htmlFor="file" className="uploadLabel">
+        Upload your PDF
+      </label>
+      <input
+        type="file"
+        name="file"
+        id="file"
+        className="fileInput"
+        {...formik.getFieldProps("file")}
+      />
+      <button type="submit" className="submitBtn">
+        Upload
+      </button>
+      {/* {formik.errors.file && formik.touched.file && (
+        <div className="uploadForm__error">{formik.errors.file}</div>
+      )} */}
+    </form>
+  );
+}
+
+export default UploadForm;
--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,49 +1,16 @@
 import React from "react";
 
-import { useFormik } from "formik";
+import GetForm from "../Components/GetForm";
+import UploadForm from "../Components/UploadForm";
 
 function Home() {
-  const formik = useFormik({
-    initialValues: {
-      file: null,
-    },
-    onSubmit: (values) => {
-      console.log(values);
-    },
-    validate: (values) => {
-      let errors = {};
-
-      if (!values.file) {
-        errors.file = "Required";
-      }
-      return errors;
-    },
-  });
   return (
     <main className="home">
       <h1 className="title">Resume.me</h1>
-      <form
-        onSubmit={formik.handleSubmit}
-        className="uploadForm"
-        name="uploadForm"
-      >
-        <label htmlFor="file" className="uploadLabel">
-          Upload your PDF
-        </label>
-        <input
-          type="file"
-          name="file"
-          id="file"
-          className="fileInput"
-          {...formik.getFieldProps("file")}
-        />
-        <button type="submit" className="submitBtn">
-          Upload
-        </button>
-        {formik.errors.file && formik.touched.file && (
-          <div className="uploadForm__error">{formik.errors.file}</div>
-        )}
-      </form>
+      <div className="features">
+        <GetForm />
+        <UploadForm />
+      </div>
     </main>
   );
 }
